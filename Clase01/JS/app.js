97a727fa725e0a19c8d93bca4509da5d04448303
crcristian97 <crcristian97.cc@gmail.com>
2024-07-09 18:14:52 -0300

    Primer Clase

---
+++ Clase01/JS/app.js
@@ -0,0 +1,36 @@
+//creacion de variable
+let ahorroCuenta; // no 
+
+// declaracion de variable
+ahorroCuenta = '6565498465'; // no 
+
+//crear la variable e inmediatamente declararla
+let edadUsuario = 18; //si
+let esMayor = true;
+
+console.log(typeof ahorroCuenta); //string
+console.log(typeof edadUsuario); //number
+console.log(typeof esMayor); // boolean
+
+//typeof para chequear los tipos de datos
+////////////////////////////
+var password = 'qwertyui';
+// funciones, ciclos , fechas , formatos, apis
+var password = 'pepitodelmonte';
+
+
+//let
+let edadUser = 25;
+// funciones, ciclos , fechas , formatos, apis
+edadUser = 36;;
+
+//const 
+//SI O SI SE DECLARA Y ASIGNA EN LA MISMA LINEA
+const IVA = 0.21;
+console.log(IVA)
+
+//prompt
+let nombreProfesor = prompt("Como te llamas");
+console.log("Bienvenido", nombreProfesor);
+
+
---
+++ Clase01/README.md
@@ -0,0 +1,10 @@
+//1-Todos los anios salen actualiazcion de JS:https://www.w3schools.com/js/js_2024.asp 
+//2-Las sentencias pueden terminar con ";" o sin. Lo recomendable es ";"
+//3-Diferencia entre var,let y const. El alcanze que tienen las variables
+//4-Tiremos siempre a usar const
+//5-3 forma de meter nuestro script de JS, recomendamos usar en el final de body
+//6-Hay que ser bien estructurados con nuestros proyectos
+//7-El metodo typoof nos permite saber que tipo de dato tenemos en nuestra variable
+//8-Por el momento usamos prompt, pero en un futuro lo eliminamos
+//9-El console.log nos ayuda a los desarrolladores a mirar nuestro codigo
+//10-El network nos permite saber si tenemos conectado nuestro app
